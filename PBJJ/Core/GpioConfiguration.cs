95ea63a66b6710a06a91e204d85d7999e18b6b65
Ben Brandt <benbrandt22@gmail.com>
2016-07-13 21:51:38 -0500

    Added static class to manage all GPIO pin configuration code

---
+++ PBJJ/Core/GpioConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.UI.Notifications;
+
+namespace PBJJ.Core
+{
+    /// <summary>
+    /// Class for keeping all the pin configuration code in one place.
+    /// Static to ensure it's only set up once.
+    /// </summary>
+    public static class GpioConfiguration
+    {
+        public static GpioPin StepperDirectionPin;
+        public static GpioPin StepperStepPin;
+
+        static GpioConfiguration()
+        {
+            // static constructor initializes pins only once the first time they are called for.
+            InitializeGpio();
+        }
+
+        private static void InitializeGpio()
+        {
+            var gpio = GpioController.GetDefault();
+
+            StepperDirectionPin = gpio.OpenPin(5);
+            StepperDirectionPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            StepperStepPin = gpio.OpenPin(13);
+            StepperStepPin.SetDriveMode(GpioPinDriveMode.Output);
+
+        }
+    }
+}
