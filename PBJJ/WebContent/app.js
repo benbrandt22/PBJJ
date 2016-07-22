ba7c655cf1f10d4c4032a9df7b0a1499afeab876
Ben Brandt <benbrandt22@gmail.com>
2016-07-21 22:28:56 -0500

    Integrated SVG based profile & blade graphic

--- PBJJ/WebContent/app.js
+++ PBJJ/WebContent/app.js
@@ -7,6 +7,7 @@
         $http.get("/api/status").then(
             function (response) {
                 home.status = response.data;
+                home.boxes = home.getDrawingBoxes(home.status.Profile.Elements);
                 $timeout(home.updateStatus, home.refreshIntervalMs);
             },
             function(error) {
@@ -23,6 +24,36 @@
         $http.post("/api/runProgram");
     };
 
+    home.getTotalWidth = function(){
+        var total = 0;
+        var totalElements = home.status.Profile.Elements.length;
+        for (var i = 0; i < totalElements; i++) {
+            var e = home.status.Profile.Elements[i];
+            total += e.Width;
+        }
+        return total;
+    };
+
+    home.drawingViewBox = function () {
+        var margin = (home.status.KerfWidthInches + 0.05);
+        var minx = (-1 * margin);
+        var miny = (-1 * margin);
+        var width = (home.getTotalWidth() + (2 * margin));
+        var height = 0.75 + (2 * margin);
+        return (minx + ' ' + miny + ' ' + width + ' ' + height);
+    };
+
+    home.getDrawingBoxes = function (elements) {
+        var x = 0;
+        var boxes = [];
+        var totalElements = elements.length;
+        for (var i = 0; i < totalElements; i++) {
+            var e = elements[i];
+            boxes.push({ x: x, y: 0, width: e.Width, height: (0.25 + (e.Type * 0.5)) });
+            x = (x + e.Width);
+        }
+        return boxes;
+    };
 
     // start the first status update
     home.updateStatus();
